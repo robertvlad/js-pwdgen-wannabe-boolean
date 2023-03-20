430899f7f7ce8ea7accf36ca737cda8910d94fb7
robertvlad <vladrob96@gmail.com>
2023-03-20 21:19:20 +0100

    Tool per generare una password semplice creato

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let name_user = prompt('Inserisci il tuo nome');
+
+let surname_user = prompt('Inserisci il tuo cognome');
+
+let color_user = prompt('Inserisci il tuo colore preferito');
+
+document.getElementById('password').innerHTML += ` ${name_user}${surname_user}${color_user}21`;
